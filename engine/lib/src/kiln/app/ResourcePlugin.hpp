5cf6df7baf4d4589d3882756093aa019e0c84a83
n0F4x <ggabor2002@gmail.com>
2026-01-23 17:46:26 +0100

    Return reference to resource when adding it to ResourcePlugin

--- engine/lib/src/kiln/app/ResourcePlugin.hpp
+++ engine/lib/src/kiln/app/ResourcePlugin.hpp
@@ -5,6 +5,7 @@
 
 #include "kiln/app/App.hpp"
 #include "kiln/util/GenericStack.hpp"
+#include "kiln/util/type_traits/result_of.hpp"
 
 namespace kiln::app {
 
@@ -23,25 +24,19 @@ concept decays_to_resource_injection_c = resource_injection_c<std::decay_t<T>>;
 class ResourcePlugin {
 public:
     template <decays_to_resource_c Resource_T>
-    auto insert_resource(Resource_T&& resource) -> void;
+    auto insert_resource(Resource_T&& resource) -> std::decay_t<Resource_T>&;
 
     template <resource_c Resource_T, typename... Args_T>
         requires std::constructible_from<Resource_T, Args_T&&...>
-    auto emplace_resource(Args_T&&... args) -> void;
+    auto emplace_resource(Args_T&&... args) -> Resource_T&;
 
     template <decays_to_resource_injection_c Injection_T>
-    auto inject_resource(Injection_T&& injection) -> void;
+    auto inject_resource(Injection_T&& injection) -> util::result_of_t<Injection_T>&;
+
 
     auto operator()(App& app) && -> void;
 
 private:
-    template <typename Resource_T>
-    struct SimpleResourceInjection {
-        Resource_T resource;
-
-        auto operator()() && -> Resource_T;
-    };
-
     util::GenericStack m_resource_stack;
 };
 
@@ -50,38 +45,33 @@ private:
 namespace kiln::app {
 
 template <decays_to_resource_c Resource_T>
-auto ResourcePlugin::insert_resource(Resource_T&& resource) -> void
+auto ResourcePlugin::insert_resource(Resource_T&& resource) -> std::decay_t<Resource_T>&
 {
-    emplace_resource<Resource_T>(std::forward<Resource_T>(resource));
+    return m_resource_stack.insert(std::forward<Resource_T>(resource));
 }
 
 template <resource_c Resource_T, typename... Args_T>
     requires std::constructible_from<Resource_T, Args_T&&...>
-auto ResourcePlugin::emplace_resource(Args_T&&... args) -> void
+auto ResourcePlugin::emplace_resource(Args_T&&... args) -> Resource_T&
 {
-    inject_resource(SimpleResourceInjection<Resource_T>{ std::forward<Args_T>(args)... });
+    return m_resource_stack.emplace<Resource_T>(std::forward<Args_T>(args)...);
 }
 
 template <decays_to_resource_injection_c Injection_T>
-auto ResourcePlugin::inject_resource(Injection_T&& injection) -> void
+auto ResourcePlugin::inject_resource(Injection_T&& injection)
+    -> util::result_of_t<Injection_T>&
 {
-    m_resource_stack.inject(std::forward<Injection_T>(injection));
+    return m_resource_stack.inject(std::forward<Injection_T>(injection));
 }
 
 inline auto ResourcePlugin::operator()(App& app) && -> void
 {
     PRECOND(
         app.resources().empty(),
-        "Resource plugin should be called before adding any resource"
+        "Resource plugin should be called before adding any resource to app"
     );
 
     app.resources() = std::move(m_resource_stack);
-}
-
-template <typename Resource_T>
-auto ResourcePlugin::SimpleResourceInjection<Resource_T>::operator()() && -> Resource_T
-{
-    return resource;
 }
 
 }   // namespace kiln::app
