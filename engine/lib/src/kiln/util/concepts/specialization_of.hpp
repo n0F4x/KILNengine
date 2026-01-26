0e7af9fdc3babe60099e7d450c234270706d9263
n0F4x <ggabor2002@gmail.com>
2026-01-26 23:49:55 +0100

    style: naming fix

--- engine/lib/src/kiln/util/concepts/specialization_of.hpp
+++ engine/lib/src/kiln/util/concepts/specialization_of.hpp
@@ -7,14 +7,17 @@ namespace kiln::util {
 namespace internal {
 
 template <typename, template <typename...> typename>
-struct is_specialization_of : std::false_type {};
+struct IsSpecializationOf : std::false_type {};
 
 template <template <typename...> typename TypeList_T, typename... Ts>
-struct is_specialization_of<TypeList_T<Ts...>, TypeList_T> : std::true_type {};
+struct IsSpecializationOf<TypeList_T<Ts...>, TypeList_T> : std::true_type {};
+
+template <template <typename> typename TypeList_T, typename T>
+struct IsSpecializationOf<TypeList_T<T>, TypeList_T> : std::true_type {};
 
 }   // namespace internal
 
 template <typename T, template <typename...> typename TypeList_T>
-concept specialization_of_c = internal::is_specialization_of<T, TypeList_T>::value;
+concept specialization_of_c = internal::IsSpecializationOf<T, TypeList_T>::value;
 
 }   // namespace kiln::util
