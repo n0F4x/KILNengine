f2bbef5afd626fb7127d1a8c358641bc5542ea49
n0F4x <ggabor2002@gmail.com>
2026-03-02 15:43:02 +0100

    app builder no longer requires to be built on the stack

--- engine/examples/events_example/main.cpp
+++ engine/examples/events_example/main.cpp
@@ -10,28 +10,24 @@ struct EventTest {
 
 auto event_test(const EventTest e) -> void
 {
-    std::println("Event test, sum:{}", e.a+e.b);
+    std::println("Event test, sum:{}", e.a + e.b);
 }
+
 auto event_test2(const EventTest e) -> void
 {
-    std::println("Event test2, diff:{}", e.a-e.b);
+    std::println("Event test2, diff:{}", e.a - e.b);
 }
 
 auto main() -> int
 {
     using namespace kiln;
 
-    // builder needs to be configured on the stack, otherwise we run into a bug on Windows
-    app::Builder builder = app::create()
-                                .insert_plugin(event::EventPlugin{});
-    app::App app = std::move(builder).build();
+    app::App app = app::create().insert_plugin(event::EventPlugin{}).build();
 
     const auto id1 =
-        app.resources().at<event::EventSystem>()
-                       .subscribe<EventTest>(event_test);
+        app.resources().at<event::EventSystem>().subscribe<EventTest>(event_test);
     const auto id2 =
-        app.resources().at<event::EventSystem>()
-                       .subscribe<EventTest>(event_test2, 1);
+        app.resources().at<event::EventSystem>().subscribe<EventTest>(event_test2, 1);
     app.resources().at<event::EventSystem>().publish<EventTest>(3, 2);
     app.resources().at<event::EventSystem>().unsubscribe<EventTest>(id1);
     app.resources().at<event::EventSystem>().publish<EventTest>(4, 1);
